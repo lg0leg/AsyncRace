5846949833bdeffa653b813d80b8d5194874b20e
lg0leg <lg.oleg1067@gmail.com>
2022-07-20 21:02:44 +0300

    feat: add router

--- async-race/src/index.tsx
+++ async-race/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './normalize.css';
 import './index.scss';
 import App from './components/app/App';
@@ -7,6 +8,8 @@ import App from './components/app/App';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
