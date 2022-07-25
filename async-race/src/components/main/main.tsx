8027d4843f5a624969b92668a2c7ccfc1df6cb44
lg0leg <lg.oleg1067@gmail.com>
2022-07-25 23:44:13 +0300

    fix: fix router path

--- async-race/src/components/main/main.tsx
+++ async-race/src/components/main/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './main.scss';
 import Garage from '../garage/garage';
 import Winners from '../winners/winners';
@@ -10,6 +10,7 @@ function Main() {
     <main className="main">
       <Routes>
         <Route index element={<Garage />} />
+        <Route path="/" element={<Navigate replace to="/garage" />} />
         <Route path="/garage" element={<Garage />} />
         <Route path="/winners" element={<Winners />} />
         <Route path="*" element={<NotFoundPage />} />
