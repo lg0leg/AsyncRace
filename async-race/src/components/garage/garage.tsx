426b620a689b6edcc05bffc4ce3f7836205a337b
lg0leg <lg.oleg1067@gmail.com>
2022-08-06 20:45:44 +0300

    feat: update pagination

--- async-race/src/components/garage/garage.tsx
+++ async-race/src/components/garage/garage.tsx
@@ -9,30 +9,43 @@ import ResetBtn from '../reset-button/reset-button';
 import GenerateCarsBtn from '../generate-cars-button/generate-cars-button';
 import Raceline from '../race-line/race-line';
 
-const items = [
-  ...Array(14)
-    .fill(0)
-    .map((item, idx) => <Raceline key={idx} name={'Audi'} />),
-];
-
 export default function Garage() {
-  const [cars, setCars] = useState<{ racelines: JSX.Element[] }>({
-    racelines: [],
+  let items: string[] = ['car1', 'car2', 'car3', 'car4', 'car5', 'car6', 'car7', 'car8', 'car9', 'car10'];
+
+  const [cars, setCars] = useState<{ arrCarNames: string[] }>({
+    arrCarNames: [],
   });
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 7;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    // @ts-ignore
+    setCurrentItems(items.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(items.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage]);
+
+  const handlePageClick = (event: { selected: number }) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length;
+    setItemOffset(newOffset);
+  };
 
   const generateCars = () => {
     let arrCarNames = [];
-    let racelines: JSX.Element[] = [];
+    // let racelines: JSX.Element[] = [];
     while (arrCarNames.length < 10) {
       let num1 = Math.floor(Math.random() * 296);
       let num2 = Math.floor(Math.random() * Object.values(carList)[num1].length);
       let Brand = Object.keys(carList)[num1];
       let Model = Object.values(carList)[num1][num2];
       arrCarNames.push(`${Brand} ${Model}`);
-      racelines = arrCarNames.map((item, idx) => <Raceline key={idx + Math.random()} name={item} />);
+      // racelines = arrCarNames.map((item, idx) => <Raceline key={idx + Math.random()} name={item} />);
     }
-    setCars({ racelines });
+    setCars({ arrCarNames });
     console.log(cars);
+    // setCars({ racelines });
   };
 
   return (
@@ -54,40 +67,12 @@ export default function Garage() {
         </h1>
       </div>
       <div className="garage-raceway">
-        <PaginatedItems itemsPerPage={7} />
+        {/* {currentItems} */}
+        {currentItems && (currentItems as string[]).map((item: string, idx: number) => <Raceline key={idx + Math.random()} name={item} />)}
       </div>
-      {/* <div className="garage-pagination"></div> */}
+      <div className="garage-pagination">
+        <ReactPaginate breakLabel="..." nextLabel="next" onPageChange={handlePageClick} pageRangeDisplayed={5} pageCount={pageCount} previousLabel="prev" renderOnZeroPageCount={() => null} />
+      </div>
     </div>
-  );
-}
-
-// @ts-ignore
-function Items({ currentItems }) {
-  return <>{currentItems && currentItems.map((item: React.FC) => item)}</>;
-}
-
-// @ts-ignore
-function PaginatedItems({ itemsPerPage }) {
-  const [currentItems, setCurrentItems] = useState(null);
-  const [pageCount, setPageCount] = useState(0);
-  const [itemOffset, setItemOffset] = useState(0);
-
-  useEffect(() => {
-    const endOffset = itemOffset + itemsPerPage;
-    // @ts-ignore
-    setCurrentItems(items.slice(itemOffset, endOffset));
-    setPageCount(Math.ceil(items.length / itemsPerPage));
-  }, [itemOffset, itemsPerPage]);
-
-  const handlePageClick = (event: { selected: number }) => {
-    const newOffset = (event.selected * itemsPerPage) % items.length;
-    setItemOffset(newOffset);
-  };
-
-  return (
-    <>
-      <Items currentItems={currentItems} />
-      <ReactPaginate breakLabel="..." nextLabel="next" onPageChange={handlePageClick} pageRangeDisplayed={5} pageCount={pageCount} previousLabel="prev" renderOnZeroPageCount={() => null} />
-    </>
   );
 }
