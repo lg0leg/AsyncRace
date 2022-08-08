522fc04c242bf27a957ea62ba60e85c836ebd2bd
lg0leg <lg.oleg1067@gmail.com>
2022-08-08 15:32:01 +0300

    feat: update colorful picker

--- async-race/src/components/create-car/create-car.tsx
+++ async-race/src/components/create-car/create-car.tsx
@@ -1,14 +1,23 @@
 import './create-car.scss';
-import ColorPicker from '../../utils/color-picker';
+// import ColorPicker from '../../utils/color-picker';
+import React, { useState } from 'react';
+import { PopoverPicker } from '../../utils/colorpick/PopoverPicker.js';
 
 export default function CreateCar() {
+  const [color, setColor] = useState('#ccbbaa');
+
+  const clickHandler = () => {
+    console.log('create color: ' + color);
+  };
+
   return (
     <div className="create-car">
       <input type="text" className="choice-name" />
       <div className="create-choice-color">
-        <ColorPicker />
+        {/* <ColorPicker /> */}
+        <PopoverPicker color={color} onChange={setColor} />
       </div>
-      <button type="button" className="create-button">
+      <button type="button" className="create-button" onClick={clickHandler}>
         Create
       </button>
     </div>
--- async-race/src/components/update-car/update-car.tsx
+++ async-race/src/components/update-car/update-car.tsx
@@ -5,13 +5,17 @@ import { PopoverPicker } from '../../utils/colorpick/PopoverPicker.js';
 export default function UpdateCar() {
   const [color, setColor] = useState('#aabbcc');
 
+  const clickHandler = () => {
+    console.log('update color: ' + color);
+  };
+
   return (
     <div className="update-car">
       <input type="text" className="choice-name" />
       <div className="update-choice-color">
         <PopoverPicker color={color} onChange={setColor} />
       </div>
-      <button type="button" className="update-button">
+      <button type="button" className="update-button" onClick={clickHandler}>
         Update
       </button>
     </div>
