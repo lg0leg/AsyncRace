2006e1e2068027cf2e2d258cd85db27d5448374d
lg0leg <lg.oleg1067@gmail.com>
2022-08-19 22:32:21 +0300

    feat: start work with api

--- async-race/src/components/race-line/race-line.tsx
+++ async-race/src/components/race-line/race-line.tsx
@@ -1,4 +1,5 @@
 import './race-line.scss';
+import ARApi from '../../utils/async-race-api';
 import { useEffect, useState } from 'react';
 import CarInstance from '../car-instance/car-instance';
 
@@ -9,6 +10,8 @@ export default function Raceline(props: {
   selectButtonHandler: (name: string, color: string) => void;
   removeButtonHandler: (name: string, color: string) => void;
 }) {
+  const Api = new ARApi();
+
   const [name, setName] = useState('');
   const [color, setColor] = useState('');
   const [start, isStarted] = useState(false);
@@ -20,6 +23,17 @@ export default function Raceline(props: {
 
   const startButtonHandler = () => {
     isStarted(true);
+  };
+
+  const stopButtonHandler = () => {
+    console.log('press stop');
+    Api.getResource('/winners')
+      .then((body) => {
+        console.log(body);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   let startClassNames = 'select-car start-car';
@@ -43,7 +57,9 @@ export default function Raceline(props: {
           <button className={startClassNames} onClick={startButtonHandler} disabled={start}>
             Start
           </button>
-          <button className="select-car stop-car">Stop</button>
+          <button className="select-car stop-car" onClick={stopButtonHandler}>
+            Stop
+          </button>
         </div>
         <div className="race-line">
           <CarInstance color={props.color} />
